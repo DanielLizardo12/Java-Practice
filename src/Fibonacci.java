8f73ccfeff8f2ce0bac26e787cf131ddfec0c15e
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-03 22:59:37 -0600

    added coments to fibonacci method and explained time and space complexity

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -4,31 +4,60 @@ import java.math.BigInteger;
  * @author Daniel
  * @Date 03/03/2023
  */
-
 public class Fibonacci {
 
-    //Given n, return the number in the fibonacci sequence
     public static void main(String[] args) {
         System.out.println(fibonacci(480000));
         System.out.println(fibonacciInN(46));
     }
 
+    /**
+     * Given n, return the number in the fibonacci sequence
+     *
+     * Time complexity: O(n)
+     *   - The algorithm iterates through n numbers in the sequence once.
+     *   - The time complexity is linear with respect to the input value n.
+     *   - For example, if we want to compute the 10th number in the sequence,
+     *   the algorithm will iterate through the sequence 10 times. If we want to compute the 20th number,
+     *   it will iterate through the sequence 20 times. And so on.
+     *
+     * Space complexity: O(1)
+     *   - The algorithm uses only constant space to store the previous two numbers in the sequence.
+     *   - The space complexity does not depend on the input value n.
+     *   - The reason for this is that the algorithm only uses three variables
+     *   (previous, current, and next) to compute the Fibonacci sequence, and these variables occupy a constant
+     *   amount of memory regardless of the value of n. This is why the space complexity is considered O(1).
+     *
+     * @param n the index of the number to return
+     * @return the nth number in the Fibonacci sequence
+     * @throws IllegalArgumentException if n is negative
+     * @throws ArithmeticException if the final number in the sequence overflows an int
+     */
     public static int fibonacciInN(int n) {
+        // Check if n is negative, and throw an exception if it is
         if (n < 0) throw new IllegalArgumentException("n must be a positive value");
+
+        // Base case: if n is 0, return 0
         if (n == 0) return 0;
 
-        //this determinate the first two numbers in the sequence
+        // Initialize variables to represent the first two numbers in the sequence
         int previous = 0;
         int current = 1;
 
         int next;
+        // Loop through the remaining n-1 numbers in the sequence
         for (int i = 2; i <= n; i++) {
+            // Calculate the next number in the sequence by adding the two previous numbers
             next = previous + current;
+            // Shift the previous numbers down the sequence
             previous = current;
             current = next;
         }
 
+        // Check if the final number in the sequence overflows an int, and throw an exception if it does
         if (current < 0) throw new ArithmeticException("Integral overflow");
+
+        // Return the final number in the sequence
 
         return current;
     }

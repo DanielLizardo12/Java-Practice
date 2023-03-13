8076391be722a99d7d2461b454f1b6ce520b63a6
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-12 19:52:47 -0600

    added test cases

--- src/practice/FindTwoSumPracticeTest.java
+++ src/practice/FindTwoSumPracticeTest.java
@@ -11,9 +11,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class FindTwoSumPracticeTest {
 
     @Test
-    public void testFindTwoSum() {
-        int[] ints = FindTwoSumPractice.findTwoSum(new int[]{1, 2, 3}, 1);
-        assertEquals(1, ints[0]);
+    public void testFindTwoSumFistTestCase() {
+        int[] testCase = FindTwoSumPractice.findTwoSum(new int[]{2, 7}, 9);
+        assertEquals(0, testCase[0]);
+        assertEquals(1, testCase[1]);
+    }
+
+    @Test
+    public void testFindTwoSumSecondTestCase() {
+        int[] testCase = FindTwoSumPractice.findTwoSum(new int[]{2, 7, 3, -1, 4}, 2);
+        assertEquals(2, testCase[0]);
+        assertEquals(3, testCase[1]);
+    }
+
+    @Test
+    public void testFindTwoSumThirdTestCase() {
+        int[] testCase = FindTwoSumPractice.findTwoSum(new int[]{2, 7, 3, -1, 4}, 100);
+        assertEquals(0, testCase.length);
+    }
+
+    @Test
+    public void testFindTwoSumFourthTestCase() {
+        int[] testCase = FindTwoSumPractice.findTwoSum(new int[]{25}, 25);
+        assertEquals(0, testCase.length);
+    }
+
+    @Test
+    public void testFindTwoSumFifthTestCase() {
+        int[] testCase = FindTwoSumPractice.findTwoSum(new int[]{}, 10);
+        assertEquals(0, testCase.length);
     }
 
 }

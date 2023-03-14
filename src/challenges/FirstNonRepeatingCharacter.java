9e8de07f654b9bdbbfd02d486c71b41d2f2e8208
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-14 13:53:42 -0600

    added unit tests

--- src/challenges/FirstNonRepeatingCharacter.java
+++ src/challenges/FirstNonRepeatingCharacter.java
@@ -5,4 +5,7 @@ package challenges;
  * @Date 14/03/2023
  */
 public class FirstNonRepeatingCharacter {
+    public static Integer firstNonRepeatingCharacter(String string){
+        return null;
+    }
 }
--- src/test/FirstNonRepeatingCharacterTest.java
+++ src/test/FirstNonRepeatingCharacterTest.java
@@ -1,9 +1,33 @@
 package test;
 
+import challenges.FirstNonRepeatingCharacter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 /**
  * @author Daniel
  * @Date 14/03/2023
  */
 class FirstNonRepeatingCharacterTest {
+
+    @Test
+    public void testFirstNonRepeatingCharacterFirstTest() {
+        Integer result = FirstNonRepeatingCharacter.firstNonRepeatingCharacter("AabcIAcbd");
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testFirstNonRepeatingCharacterSecondTest() {
+        Integer result = FirstNonRepeatingCharacter.firstNonRepeatingCharacter("aaAA33");
+        assertNull(result);
+    }
+
+    @Test
+    public void testFirstNonRepeatingCharacterThirdTest() {
+        Integer result = FirstNonRepeatingCharacter.firstNonRepeatingCharacter("abc");
+        assertEquals(0, result);
+    }
 
 }

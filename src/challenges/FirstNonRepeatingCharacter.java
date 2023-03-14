4cf76093d253cb16f8c576db0d374d71b4ba1bc3
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-14 14:35:46 -0600

    added modified algorith

--- src/challenges/FirstNonRepeatingCharacter.java
+++ src/challenges/FirstNonRepeatingCharacter.java
@@ -1,20 +1,24 @@
 package challenges;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Daniel
  * @Date 14/03/2023
  */
 public class FirstNonRepeatingCharacter {
     public static Integer firstNonRepeatingCharacter(String string){
+        Map<Character, Integer> frequency = new HashMap<>();
         for (int i = 0; i < string.length(); i++) {
-            int counter = 0;
-            for (int j = 0; j < string.length(); j++) {
-                if ((string.charAt(i) == string.charAt(j)) && (i != j)) {
-                    counter++;
-                    System.out.println(i + " "  + j);
-                }
+            char c = string.charAt(i);
+            frequency.put(c, frequency.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (frequency.get(c) == 1) {
+                return i;
             }
-            if (counter == 0) return i;
         }
         return null;
     }

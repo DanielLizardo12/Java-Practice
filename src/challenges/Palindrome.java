3ce14d967abf4c5730aa4dac729ee5233efdf074
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-14 15:31:28 -0600

    added palindrome solution

--- src/challenges/Palindrome.java
+++ src/challenges/Palindrome.java
@@ -5,4 +5,13 @@ package challenges;
  * @Date 14/03/2023
  */
 public class Palindrome {
+
+    public static boolean palindrome(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)){
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/PalindromeTest.java
+++ src/test/PalindromeTest.java
@@ -1,11 +1,40 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import challenges.Palindrome;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author Daniel
  * @Date 14/03/2023
  */
 class PalindromeTest {
+
+    @Test
+    public void testPalindromeFistTest() {
+        assertTrue(Palindrome.palindrome("anitalavalatina"));
+    }
+
+    @Test
+    public void testPalindromeSecondTest() {
+        assertTrue(Palindrome.palindrome("malayalam"));
+    }
+
+    @Test
+    public void testPalindromeThirdTest() {
+        assertTrue(Palindrome.palindrome("aa"));
+    }
+
+    @Test
+    public void testPalindromeFourthTest() {
+        assertTrue(Palindrome.palindrome("b"));
+    }
+    @Test
+    public void testPalindromeFifthTest() {
+        assertFalse(Palindrome.palindrome("aA"));
+    }
 
 }

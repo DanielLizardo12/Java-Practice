a0b55f04cf769d87bb017ab000deef63b8401535
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-15 17:38:57 -0600

    added gropu anagrams challenge

---
+++ src/challenges/GroupAnagrams.java
@@ -0,0 +1,15 @@
+package challenges;
+
+import java.util.List;
+
+/**
+ * @author Daniel
+ * @Date 15/03/2023
+ */
+public class GroupAnagrams {
+
+    public static List<List<String>> groupAnagrams(String[] strs){
+        return null;
+    }
+
+}

743f8f4a496bf4a696afa986e7fbdeb012112e64
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-16 14:36:52 -0600

    added binary search challenge

---
+++ src/challenges/BinarySearch.java
@@ -0,0 +1,13 @@
+package challenges;
+
+/**
+ * @author Daniel
+ * @Date 16/03/2023
+ */
+
+public class BinarySearch {
+
+    public static int search(int[] ints, int target) {
+        return -1;
+    }
+}

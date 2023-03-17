9c05b5e0850fb2324b373393ffe3f18111cf8ee4
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-16 18:11:43 -0600

    added solution to sorted binary search

--- src/challenges/SortedBinarySearch.java
+++ src/challenges/SortedBinarySearch.java
@@ -8,6 +8,27 @@ package challenges;
 public class SortedBinarySearch {
 
     public static int search(int[] ints, int target) {
+        int left = 0;
+        int right = ints.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (ints[middle] == target) {
+                return middle;
+            } else if (ints[left] <= ints[middle]) {
+                if (ints[left] <= target && target < ints[middle]) {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+            } else {
+                if (ints[middle] < target && target <= ints[right]) {
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
+            }
+        }
         return -1;
     }
 }

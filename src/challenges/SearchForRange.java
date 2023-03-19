771cf78de42ced707726a520fe65dd7dc049ebb8
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-18 18:01:17 -0600

    added searchForRange challenge

---
+++ src/challenges/SearchForRange.java
@@ -0,0 +1,14 @@
+package challenges;
+
+/**
+ * @author Daniel
+ * @Date 18/03/2023
+ */
+
+public class SearchForRange {
+
+    public static int[] searchRange(int[] ints, int target) {
+        return new int[]{};
+    }
+
+}

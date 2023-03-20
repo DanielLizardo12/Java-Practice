834630b59674bfe1f20d7aba4be5af6cac63b1b7
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-20 13:34:43 -0600

    added search matrix challenge solution

--- src/challenges/SearchInMatrix.java
+++ src/challenges/SearchInMatrix.java
@@ -1,5 +1,7 @@
 package challenges;
 
+import java.util.Arrays;
+
 /**
  * @author Daniel
  * @Date 19/03/2023
@@ -8,7 +10,31 @@ package challenges;
 public class SearchInMatrix {
 
     public static boolean searchMatrix(int[][] matrix, int target) {
-        return true;
+        for (int[] ints : matrix) {
+            if (ints[ints.length - 1] >= target) {
+                return search(ints, target);
+            }
+        }
+        return false;
+    }
+
+    public static boolean search(int[] ints, int target) {
+        int left = 0;
+        int right = ints.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (ints[middle] == target) {
+                System.out.printf("intern Array = %s, index at array = %s%n",
+                        Arrays.toString(ints), middle);
+                return true;
+            } else if (ints[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return false;
     }
 
 }
--- src/test/SearchInMatrixTest.java
+++ src/test/SearchInMatrixTest.java
@@ -3,6 +3,7 @@ package test;
 import challenges.SearchInMatrix;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -12,9 +13,35 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SearchInMatrixTest {
 
+    int[][] matrix = new int[][]{
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 60}
+    };
+
     @Test
     public void testSearchMatrixFistTest() {
-        assertTrue(SearchInMatrix.searchMatrix(null, 1));
+        assertTrue(SearchInMatrix.searchMatrix(matrix, 3));
+    }
+
+    @Test
+    public void testSearchMatrixSecondTest() {
+        assertTrue(SearchInMatrix.searchMatrix(matrix, 16));
+    }
+
+    @Test
+    public void testSearchMatrixThirdTest() {
+        assertTrue(SearchInMatrix.searchMatrix(matrix, 60));
+    }
+
+    @Test
+    public void testSearchMatrixFourthTest() {
+        assertFalse(SearchInMatrix.searchMatrix(matrix, 0));
+    }
+
+    @Test
+    public void testSearchMatrixFifthTest() {
+        assertFalse(SearchInMatrix.searchMatrix(matrix, 200));
     }
 
 }

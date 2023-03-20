57e372adc1145670ae8a4217e407fc7d7634334f
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-20 16:45:16 -0600

    added insertion sort solution

--- src/challenges/InsertionSort.java
+++ src/challenges/InsertionSort.java
@@ -7,8 +7,27 @@ package challenges;
 
 public class InsertionSort {
 
+    // It has a time complexity of O(n^2) and a space complexity of O(1).
     public static int[] insertionSort(int[] array) {
-        return new int[]{};
+        int temp;   // variable to hold the current element being inserted
+        int j;      // index of the last element in the sorted subarray
+
+        for (int i = 1; i < array.length; i++) {
+            j = i - 1;
+            temp = array[i];
+
+            // Shift elements in the sorted subarray to the right until the correct position for the current element is found
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            // Insert the current element into its correct position in the sorted subarray
+            array[j + 1] = temp;
+        }
+
+        // Return the sorted array
+        return array;
     }
 
 }

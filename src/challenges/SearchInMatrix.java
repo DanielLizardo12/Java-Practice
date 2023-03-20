62f6c62f49b8e634bd19fa1f665b698c8eb32f3b
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-19 20:55:21 -0600

    added search matrix challenge

---
+++ src/challenges/SearchInMatrix.java
@@ -0,0 +1,14 @@
+package challenges;
+
+/**
+ * @author Daniel
+ * @Date 19/03/2023
+ */
+
+public class SearchInMatrix {
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        return true;
+    }
+
+}

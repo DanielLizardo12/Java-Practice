6c9339b97b6c0b7d69e59e683b607a1e129fe5ee
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-20 16:23:48 -0600

    added insertion sort challenge

---
+++ src/challenges/InsertionSort.java
@@ -0,0 +1,14 @@
+package challenges;
+
+/**
+ * @author Daniel
+ * @Date 20/03/2023
+ */
+
+public class InsertionSort {
+
+    public static int[] insertionSort(int[] array) {
+        return new int[]{};
+    }
+
+}
---
+++ src/test/InsertionSortTest.java
@@ -0,0 +1,8 @@
+package test;
+/**
+ * @author Daniel
+ * @Date 20/03/2023
+ */
+class InsertionSortTest {
+
+}

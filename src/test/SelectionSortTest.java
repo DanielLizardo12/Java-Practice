fcd370d1d20835760eb7d886d92b0005b1548ea0
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-21 20:43:14 -0600

    added unit tests to selection sort

--- src/test/SelectionSortTest.java
+++ src/test/SelectionSortTest.java
@@ -20,4 +20,43 @@ class SelectionSortTest {
         assertEquals(3, result[2]);
     }
 
+    @Test
+    public void testSelectionSortSecondTest() {
+        int[] result = SelectionSort.selectionSort(new int[]{3, 2, 2, 3, 4, 5, 7, 7, 7, 1});
+
+        assertEquals(1, result[0]);
+        assertEquals(2, result[1]);
+        assertEquals(2, result[2]);
+        assertEquals(3, result[3]);
+        assertEquals(3, result[4]);
+        assertEquals(4, result[5]);
+        assertEquals(5, result[6]);
+        assertEquals(7, result[7]);
+        assertEquals(7, result[8]);
+        assertEquals(7, result[9]);
+    }
+
+    @Test
+    public void testSelectionSortThirdTest() {
+        int[] result = SelectionSort.selectionSort(new int[]{});
+
+        assertEquals(0, result.length);
+    }
+
+    @Test
+    public void testSelectionSortFourthTest() {
+        int[] result = SelectionSort.selectionSort(new int[]{-100, 300, 342, 222, -123123, 1, -1, 8, 8, 0});
+
+        assertEquals(-123123, result[0]);
+        assertEquals(-100, result[1]);
+        assertEquals(-1, result[2]);
+        assertEquals(0, result[3]);
+        assertEquals(1, result[4]);
+        assertEquals(8, result[5]);
+        assertEquals(8, result[6]);
+        assertEquals(222, result[7]);
+        assertEquals(300, result[8]);
+        assertEquals(342, result[9]);
+    }
+
 }

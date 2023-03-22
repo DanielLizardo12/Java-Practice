898a5a5ea22b1e3eea1db46205c98226cfcff5b2
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-22 14:34:49 -0600

    added merge sort challenge

---
+++ src/challenges/MergeSort.java
@@ -0,0 +1,14 @@
+package challenges;
+
+/**
+ * @author Daniel
+ * @Date 22/03/2023
+ */
+
+public class MergeSort {
+
+    public static int[] mergeSort(int[] firstArray, int[] secondArray) {
+        return new int[]{};
+    }
+
+}

c4592a90ae91578c570a9e958b59b208bbefaaac
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-21 21:38:08 -0600

    added selection sort solution

--- src/challenges/SelectionSort.java
+++ src/challenges/SelectionSort.java
@@ -1,5 +1,8 @@
 package challenges;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * @author Daniel
  * @Date 21/03/2023
@@ -8,7 +11,20 @@ package challenges;
 public class SelectionSort {
 
     public static int[] selectionSort(int[] array) {
-        return new int[]{};
+        for (int i = 0; i < array.length - 1; i++) {
+            // Find the index of the smallest element in the unsorted part of the array
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the smallest element with the first element of the unsorted part of the array
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+        // Return the sorted array
+        return array;
     }
-
 }

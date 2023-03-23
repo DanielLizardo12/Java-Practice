ae3c65e875f19479b0735b7ec5eaccd90e0a1d06
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-23 14:18:51 -0600

    added linked list challenge

---
+++ src/challenges/linkedList/MyLinkedList.java
@@ -0,0 +1,33 @@
+package challenges.linkedList;
+
+/**
+ * @author Daniel
+ * @Date 23/03/2023
+ */
+
+public class MyLinkedList {
+
+    public MyLinkedList() {
+
+    }
+
+    public int get(int index) {
+        return 0;
+    }
+
+    public void addAtHead(int val) {
+
+    }
+
+    public void addTail(int tail) {
+
+    }
+
+    public void addAtIndex(int index, int val) {
+
+    }
+
+    public void deleteAtIndex(int index) {
+
+    }
+}

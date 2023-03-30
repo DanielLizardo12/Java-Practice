4a5d4a235eafd028ff8a0eda1db640b657f7458d
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-30 15:23:20 -0600

    added myQueue solution

--- src/challenges/MyQueue.java
+++ src/challenges/MyQueue.java
@@ -1,5 +1,7 @@
 package challenges;
 
+import java.util.Stack;
+
 /**
  * @author Daniel
  * @Date 30/03/2023
@@ -7,23 +9,53 @@ package challenges;
 
 public class MyQueue {
 
-    public MyQueue() {
+    private Stack<Integer> firstStack;
+    private Stack<Integer> secondStack;
+    private int firstElement;
 
+    public MyQueue() {
+        firstStack = new Stack<>();
+        secondStack = new Stack<>();
+        firstElement = -1;
     }
 
     public void push(int x) {
-
+        if (firstStack.isEmpty()) {
+            firstElement = x;
+        }
+        firstStack.push(x);
     }
 
     public int pop() {
+        if (secondStack.isEmpty()) {
+            while (!firstStack.isEmpty()) {
+                secondStack.push(firstStack.pop());
+            }
+        }
 
+        int poppedElement = secondStack.pop();
+        if (secondStack.isEmpty()) {
+            firstElement = -1;
+        } else {
+            firstElement = secondStack.peek();
+        }
+        return poppedElement;
     }
 
     public int peek() {
-
+        if (secondStack.isEmpty()) {
+            while (!firstStack.isEmpty()) {
+                secondStack.push(firstStack.pop());
+            }
+        }
+        if (secondStack.isEmpty()) {
+            return -1; // queue is empty
+        } else {
+            return secondStack.peek();
+        }
     }
 
     public boolean empty() {
-
+        return firstStack.isEmpty() && secondStack.isEmpty();
     }
 }

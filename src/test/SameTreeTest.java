d13d8944cdc5c6f8a62a3db93d3d85afc9598ad0
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-31 19:02:18 -0600

    added initial testing

--- src/test/SameTreeTest.java
+++ src/test/SameTreeTest.java
@@ -1,11 +1,22 @@
 package test;
 
+import challenges.SameTree;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 /**
  * @author Daniel
  * @Date 31/03/2023
  */
 
 class SameTreeTest {
-    
+
+    @Test
+    public void testSameTree() {
+        SameTree.TreeNode p = new SameTree.TreeNode(1, null, null);
+        SameTree.TreeNode q = new SameTree.TreeNode(1, null, null);
+
+        Assertions.assertTrue(SameTree.isSameTree(p, q));
+    }
 
 }

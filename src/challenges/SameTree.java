b8293e8130ecd2c371e46450d242454a93798d40
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-03-31 18:56:27 -0600

    added sameTree challenge

---
+++ src/challenges/SameTree.java
@@ -0,0 +1,25 @@
+package challenges;
+
+/**
+ * @author Daniel
+ * @Date 31/03/2023
+ */
+
+public class SameTree {
+
+
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+}
---
+++ src/test/SameTreeTest.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author Daniel
+ * @Date 31/03/2023
+ */
+
+class SameTreeTest {
+    
+
+}

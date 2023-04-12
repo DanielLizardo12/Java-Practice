f750e92f7ea9a0f748776aba404931e20a325b11
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-12 17:09:34 -0600

    added simple unit test

--- src/challenges/ValidateSubSequence.java
+++ src/challenges/ValidateSubSequence.java
@@ -1,9 +1,15 @@
 package challenges;
 
+import java.util.List;
+
 /**
  * @author Daniel
  * @Date 12/04/2023
  */
 public class ValidateSubSequence {
 
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+
+        return false;
+    }
 }
--- src/test/ValidateSubSequenceTest.java
+++ src/test/ValidateSubSequenceTest.java
@@ -1,11 +1,24 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import challenges.ValidateSubSequence;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 /**
  * @author Daniel
  * @Date 12/04/2023
  */
+
 class ValidateSubSequenceTest {
+
+    @Test
+    public void validateSubSequenceSimpleInnerArray() {
+        List<Integer> array = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> innerArray = List.of(2, 4);
+
+        Assertions.assertTrue(ValidateSubSequence.isValidSubsequence(array, innerArray));
+    }
 
 }

b643043b4af220e6ff8045f0d36dee38a82c377a
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-12 17:07:06 -0600

    added validateSubSequence challenge

---
+++ src/challenges/ValidateSubSequence.java
@@ -0,0 +1,9 @@
+package challenges;
+
+/**
+ * @author Daniel
+ * @Date 12/04/2023
+ */
+public class ValidateSubSequence {
+
+}
---
+++ src/test/ValidateSubSequenceTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Daniel
+ * @Date 12/04/2023
+ */
+class ValidateSubSequenceTest {
+
+}

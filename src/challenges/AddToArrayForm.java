2d579882fa5b1c878bcb57392fdd36a1c5acdc2f
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-11 19:27:51 -0600

    added addToArrayForm challenge

---
+++ src/challenges/AddToArrayForm.java
@@ -0,0 +1,16 @@
+package challenges;
+
+import java.util.List;
+
+/**
+ * @author Daniel
+ * @Date 11/04/2023
+ */
+
+public class AddToArrayForm {
+
+    public static List<Integer> addToArrayForm(int[] num, int k) {
+        return null;
+    }
+
+}
---
+++ src/test/AddToArrayFormTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Daniel
+ * @Date 11/04/2023
+ */
+
+class AddToArrayFormTest {
+
+    @Test
+    public void testAddToArraySimpleSum() {
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+
+    }
+
+}

0c03bd8bb182e588b93f231af6ccdbcfe1e12431
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-14 15:14:06 -0600

    added all unit testing

--- src/test/AreNumbersAscendingTest.java
+++ src/test/AreNumbersAscendingTest.java
@@ -20,6 +20,18 @@ class AreNumbersAscendingTest {
     @Test
     public void testAreNumbersAscendingBox() {
         String input = "1 box has 3 blue 4 red 6 green and 12 yellow marbles";
+        Assertions.assertTrue(AreNumbersAscending.areNumbersAscending(input));
+    }
+
+    @Test
+    public void testAreNumbersAscendingHelloWorld() {
+        String input = "hello world 5 x 5";
+        Assertions.assertFalse(AreNumbersAscending.areNumbersAscending(input));
+    }
+
+    @Test
+    public void testAreNumbersAscendingSunset() {
+        String input = "sunset is at 7 51 pm overnight lows will be in the low 50 and 60 s";
         Assertions.assertFalse(AreNumbersAscending.areNumbersAscending(input));
     }
 

28e8d72206abac38a0c285000170805fd940f3ea
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-14 15:09:36 -0600

    added are numbers ascending challenge class and unit testing class

---
+++ src/challenges/AreNumbersAscending.java
@@ -0,0 +1,12 @@
+package challenges;
+
+/**
+ * @author Daniel
+ * @Date 14/04/2023
+ */
+
+public class AreNumbersAscending {
+
+
+
+}
---
+++ src/test/AreNumbersAscendingTest.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * @author Daniel
+ * @Date 14/04/2023
+ */
+
+class AreNumbersAscendingTest {
+
+}

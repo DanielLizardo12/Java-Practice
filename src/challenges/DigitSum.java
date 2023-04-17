617b00c2ecf2dc342c7f062fbae62da8efcade09
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-17 11:40:12 -0600

    added digitSum challenge

---
+++ src/challenges/DigitSum.java
@@ -0,0 +1,12 @@
+package challenges;
+
+/**
+ * @author Daniel
+ * @Date 17/04/2023
+ */
+
+public class DigitSum {
+
+
+
+}
---
+++ src/test/DigitSumTest.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * @author Daniel
+ * @Date 17/04/2023
+ */
+
+class DigitSumTest {
+
+}

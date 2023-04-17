dd36efbaa020ffe5dd26790ba8a9ebcdfea01670
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-17 12:19:25 -0600

    added digitSum solution

--- src/challenges/DigitSum.java
+++ src/challenges/DigitSum.java
@@ -1,5 +1,9 @@
 package challenges;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author Daniel
  * @Date 17/04/2023
@@ -8,7 +12,41 @@ package challenges;
 public class DigitSum {
 
     public static String digitSum(String s, int k) {
-        return "string";
+
+        if (s.length() <= k) return s;
+
+        String stringAfterRound = "";
+
+        List<String> stringList = subStringByk(s, k);
+
+        stringAfterRound += stringList.stream().map(DigitSum::summedSubString).collect(Collectors.joining());
+
+
+        return digitSum(stringAfterRound, k);
     }
 
+    private static String summedSubString(String s) {
+        int summedString = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            summedString += Integer.parseInt(String.valueOf(s.charAt(i)));
+        }
+
+        return String.valueOf(summedString);
+    }
+
+    private static List<String> subStringByk(String s, int k) {
+        List<String> stringList = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i += k) {
+            if (i + k < s.length()) {
+                stringList.add(s.substring(i, i + k));
+            } else {
+                stringList.add(s.substring(i));
+            }
+
+        }
+
+        return stringList;
+    }
 }

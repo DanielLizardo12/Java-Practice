5b682a89fc6276ffb1ad0b3662eb43f94797638e
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-19 09:30:42 -0600

    added initial testing

--- src/challenges/SameTree.java
+++ src/challenges/SameTree.java
@@ -26,7 +26,7 @@ public class SameTree {
         TreeNode left;
         TreeNode right;
         public TreeNode() {}
-        TreeNode(int val) { this.val = val; }
+        public TreeNode(int val) { this.val = val; }
         public TreeNode(int val, TreeNode left, TreeNode right) {
           this.val = val;
           this.left = left;
--- src/test/SymmetricTreeTest.java
+++ src/test/SymmetricTreeTest.java
@@ -1,10 +1,26 @@
 package test;
 
+import challenges.SameTree;
+import challenges.SymmetricTree;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 /**
  * @author Daniel
  * @Date 19/04/2023
  */
 
 class SymmetricTreeTest {
+
+    @Test
+    public void testSymmetricTree() {
+        SameTree.TreeNode treeNodeFour = new SameTree.TreeNode(4);
+        SameTree.TreeNode treeNodeThree = new SameTree.TreeNode(3);
+        SameTree.TreeNode treeNodeTwoLeft = new SameTree.TreeNode(2, treeNodeThree, treeNodeFour);
+        SameTree.TreeNode treeNodeTwoRight = new SameTree.TreeNode(2, treeNodeFour, treeNodeThree);
+        SameTree.TreeNode treeNode = new SameTree.TreeNode(1, treeNodeTwoLeft, treeNodeTwoRight);
+
+        Assertions.assertTrue(SymmetricTree.isSymmetric(treeNode));
+    }
 
 }

dd4b8cfdfa9bbfd952a5fd7e43a032d6d0b4490a
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-19 09:41:51 -0600

    added solution to isSymmetric tree

--- src/challenges/SymmetricTree.java
+++ src/challenges/SymmetricTree.java
@@ -8,6 +8,17 @@ package challenges;
 public class SymmetricTree {
 
     public static boolean isSymmetric(SameTree.TreeNode root) {
-        return false;
+        if (root == null) return true;
+
+        return isMirror(root.left, root.right);
+    }
+
+    private static boolean isMirror(SameTree.TreeNode left, SameTree.TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+
+        return left.val == right.val
+                && isMirror(left.left, right.right)
+                && isMirror(left.right, right.left);
     }
 }

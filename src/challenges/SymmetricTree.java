45b408e52cc0ad9c7de50660961e51e69fd5e2e8
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-19 09:25:04 -0600

    added Symmetric tree challenge

---
+++ src/challenges/SymmetricTree.java
@@ -0,0 +1,13 @@
+package challenges;
+
+/**
+ * @author Daniel
+ * @Date 19/04/2023
+ */
+
+public class SymmetricTree {
+
+    public static boolean isSymmetric(SameTree.TreeNode root) {
+        return false;
+    }
+}
---
+++ src/test/SymmetricTreeTest.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * @author Daniel
+ * @Date 19/04/2023
+ */
+
+class SymmetricTreeTest {
+
+}

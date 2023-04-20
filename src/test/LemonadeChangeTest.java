1f46156c9349a1b6d454d5a2db3e3ecd5808334b
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-20 11:51:36 -0600

    added initial testing

--- src/test/LemonadeChangeTest.java
+++ src/test/LemonadeChangeTest.java
@@ -1,10 +1,19 @@
 package test;
 
+import challenges.LemonadeChange;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 /**
  * @author Daniel
  * @Date 20/04/2023
  */
 
 class LemonadeChangeTest {
+
+    @Test
+    public void testLemonadeChangeCorrectChange() {
+        Assertions.assertTrue(LemonadeChange.lemonadeChange(new int[]{5, 5, 5, 10, 20}));
+    }
 
 }

5a045ca248a3da71302888a272d0a49e7795cbc0
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-20 11:50:02 -0600

    added lemonade change challenge

---
+++ src/challenges/LemonadeChange.java
@@ -0,0 +1,14 @@
+package challenges;
+
+/**
+ * @author Daniel
+ * @Date 20/04/2023
+ */
+
+public class LemonadeChange {
+
+    public static boolean lemonadeChange(int[] bills) {
+        return false;
+    }
+
+}
---
+++ src/test/LemonadeChangeTest.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * @author Daniel
+ * @Date 20/04/2023
+ */
+
+class LemonadeChangeTest {
+
+}

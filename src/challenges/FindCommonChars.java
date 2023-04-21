e8d75124339de76baeb53712d5a25a3688e42658
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-21 10:43:22 -0600

    added findCommonChars challenge

---
+++ src/challenges/FindCommonChars.java
@@ -0,0 +1,16 @@
+package challenges;
+
+import java.util.List;
+
+/**
+ * @author Daniel
+ * @Date 21/04/2023
+ */
+
+public class FindCommonChars {
+
+    public static List<String> commonChars(String[] words) {
+
+    }
+
+}
---
+++ src/test/FindCommonCharsTest.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * @author Daniel
+ * @Date 21/04/2023
+ */
+
+class FindCommonCharsTest {
+
+}

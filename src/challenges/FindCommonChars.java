a782db5f75681e477ed4d0a299d09d952be9a9cd
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-21 11:06:35 -0600

    added solution

--- src/challenges/FindCommonChars.java
+++ src/challenges/FindCommonChars.java
@@ -1,5 +1,6 @@
 package challenges;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,7 +11,27 @@ import java.util.List;
 public class FindCommonChars {
 
     public static List<String> commonChars(String[] words) {
+        int vt[][]= new int[26][3];
+        for(int i=0;i<26;i++)
+            vt[i][1]=1000000;
 
+
+        int n=words.length;
+        for(String st:words){
+
+            for(char c:st.toCharArray()){
+                vt[c-'a'][0]++;
+                vt[c-'a'][2]++;
+            }
+            for(int i=0;i<26;i++){
+                vt[i][1]=Math.min(vt[i][1],vt[i][2]);vt[i][2]=0;}
+
+        }
+        List<String> list=new ArrayList<>();
+        for(int i=0;i<26;i++){
+            while(vt[i][0]>=n && vt[i][1]-->0)list.add(String.valueOf((char)(97+i)));}
+
+        return list;
     }
 
 }

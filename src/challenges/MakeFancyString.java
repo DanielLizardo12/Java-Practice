2e2f4e8474c5104b5a44c4a9f37ea9b534616c27
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-22 20:14:10 -0600

    added makefancystring challenfe

---
+++ src/challenges/MakeFancyString.java
@@ -0,0 +1,10 @@
+package challenges;
+
+/**
+ * @author Daniel
+ * @Date 22/04/2023
+ */
+
+public class MakeFancyString {
+
+}
---
+++ src/test/MakeFancyStringTest.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * @author Daniel
+ * @Date 22/04/2023
+ */
+
+class MakeFancyStringTest {
+
+}

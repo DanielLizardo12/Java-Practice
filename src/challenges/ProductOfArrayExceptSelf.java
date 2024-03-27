ad9ef0a8c99c8638b4c8fe12b31cf695613f435a
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2024-03-27 17:02:07 -0600

    added initial approach for ProductOfArrayExceptSelf

--- src/challenges/ProductOfArrayExceptSelf.java
+++ src/challenges/ProductOfArrayExceptSelf.java
@@ -11,22 +11,33 @@ import java.util.Arrays;
 public class ProductOfArrayExceptSelf {
 
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(productExceptSelf(new int[]{1,2,3,4})));
+    System.out.println(Arrays.toString(productExceptSelfInitialApproach(new int[]{1,2,3,4})));
   }
 
-  private static int[] productExceptSelf(int[] nums) {
+  private static int[] productExceptSelfInitialApproach(int[] nums) {
+    final int arrayLength = nums.length;
+    int[] productArray =  new int[arrayLength];
 
-    int productOfEntireArray = 1;
+    for (int i = 0; i < arrayLength; i++) {
+      if (i == 0) {
+        productArray[0] = calculateProductOfInnerArray(nums, 1, arrayLength);
+      } else if (i == arrayLength - 1) {
+        productArray[arrayLength - 1] = calculateProductOfInnerArray(nums, 0, arrayLength - 1);
+      } else {
+        productArray[i] = calculateProductOfInnerArray(nums, 0, i)
+            * calculateProductOfInnerArray(nums, i + 1, arrayLength);
+      }
+    }
+    return productArray;
+  }
 
-    for (int num : nums) {
-      productOfEntireArray = productOfEntireArray * num;
+  private static int calculateProductOfInnerArray(int[] nums, int firstIndex, int lastIndex) {
+    int product = 1;
+    for (int i = firstIndex; i < lastIndex; i++) {
+      product = product * nums[i];
     }
 
-    for (int i = 0; i < nums.length; i++) {
-      nums[i] = productOfEntireArray / nums[i];
-    }
-
-    return nums;
+    return product;
   }
 
 }

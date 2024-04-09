14b6e82d1f09a01e12133c5e909ee1b2e8bd7a6d
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2024-04-09 14:27:25 -0600

    added TowerOfHanoi initial code

---
+++ src/challenges/TowerOfHanoi.java
@@ -0,0 +1,21 @@
+package challenges;
+
+/**
+ * TowerOfHanoi description.
+ *
+ * @author Daniel
+ * @version 08/04/2024
+ */
+public class TowerOfHanoi {
+
+  public static void main(String[] args) {
+
+  }
+
+  private static void hanoiGame() {
+    int[] towerA = new int[]{1,2,3,4,5};
+    int[] towerB = new int[5];
+    int[] towerC = new int[5];
+  }
+
+}

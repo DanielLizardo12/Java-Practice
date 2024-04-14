0d9d09e2e953881376fefe1c21a30c291bfe7060
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2024-04-13 19:23:30 -0600

    added hashmap of objects

--- src/lambdaPractice/Main.java
+++ src/lambdaPractice/Main.java
@@ -2,7 +2,11 @@ package lambdaPractice;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Main description.
@@ -30,7 +34,7 @@ public class Main {
       }
     };
 
-    Executor executor1 = (strings) -> {
+    Executor executor1 = strings -> {
       for (String string: strings) {
         if (string.equals("wd")) {
           System.out.println(string);
@@ -48,8 +52,34 @@ public class Main {
     int[] numbers = new int[]{1,1,2,2,2,2,2,3,4,4,4,4,5,6,7,8,9,10};
 
     Arrays.stream(numbers)
+        .boxed()
+        .collect(Collectors.toSet())
+        .stream()
         .filter(number -> number % 2 == 0)
         .forEach(System.out::println);
+
+    Cat cat1 = new Cat(1,"JimmyMcgill");
+    Cat cat2 = new Cat(2,"KimWexler");
+    Cat cat3 = new Cat(1,"JimmyMcgill");
+    Cat cat4 = new Cat(2,"KimWexler");
+
+
+    List<Cat> cats = new ArrayList<>();
+    cats.add(cat1);
+    cats.add(cat2);
+    cats.add(cat3);
+    cats.add(cat4);
+
+    HashMap<Integer, Cat> catHashMap = new HashMap<>();
+
+    for (Cat cat : cats) {
+      catHashMap.put(cat.getId(), cat);
+    }
+
+    List<Cat> uniqueCats = new ArrayList<>(catHashMap.values());
+
+    //System.out.println(catHashMap.values());
+    System.out.println(uniqueCats);
   }
 
 }
---
+++ src/topInterview150/challenge/IsomorphicStrings.java
@@ -0,0 +1,23 @@
+package topInterview150.challenge;
+
+/**
+ * IsomorphicStrings description.
+ *
+ * @author Daniel
+ * @version 12/04/2024
+ */
+public class IsomorphicStrings {
+
+  public static void main(String[] args) {
+    System.out.println(isIsomorphic("egg", "add"));
+  }
+
+  private static boolean isIsomorphic(String s, String t) {
+
+
+    return true;
+  }
+
+}
+
+

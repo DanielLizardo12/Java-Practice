7b589408cc2f6e372baf80f8913442e9551fdee9
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2025-07-25 14:17:26 -0600

    Add Main.java with basic main method
    added mainpractice method to test quickly

---
+++ src/mainPractice/Main.java
@@ -0,0 +1,15 @@
+package mainPractice;
+
+/**
+ * Main description.
+ *
+ * @author Daniel
+ * @version 25/07/2025
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("hello");
+  }
+
+}
